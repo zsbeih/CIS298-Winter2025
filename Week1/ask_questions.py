09b2da3c1d99fbd03678cf41153958bfd4f5ee16
zsbeih <zsbeih@umich.edu>
2025-01-10 13:44:19 -0500

    Project 0 ask questions program

---
+++ Week1/ask_questions.py
@@ -0,0 +1,7 @@
+name = 'Mohammad'
+major = 'Software Engineering'
+
+print(f'The friend I met in this class goes by the name: {name} and is majored in {major}.')
+print("Outside of school they like to work at a restaurant and play video games.")
+print("A fun fact about them is they design websites on the side.")
+print("They told me the coolest thing they have seen technology do is object detection with AI.")
